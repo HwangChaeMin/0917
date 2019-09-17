743f2ca3c767b360776eed1eee053b7378ffd43d
HwangChaeMin <54874499+HwangChaeMin@users.noreply.github.com>
2019-09-16 23:25:32 -0700

    Add files via upload

---
+++ 0917/fopen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc,char *argv[]){
+  int fd;
+   if((fd = open(argv[1],O_RDWR)) == -1)
+   printf("file open erroe\n");
+  else printf("file %s open : %d\n",argv[1],fd);
+ close(fd);
+ exit(0);
+}
